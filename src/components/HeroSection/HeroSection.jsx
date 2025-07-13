acafb71988685b794b3305db1f311edcaddaf507
Augusto <augustocesarrezende64@gmail.com>
2025-07-13 10:55:41 -0300

    ajustes no css

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import './HeroSection.css'
+import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
-
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
     if (element) {
@@ -14,35 +13,39 @@ const HeroSection = () => {
   };
 
   return (
-    <section id='hero' className='hero-container'>
-      <div className='hero-content'>
-        <div className='hero-text'>
+    <section id="hero" className="hero-container">
+      <div className="hero-content">
+        <div className="hero-text">
           <h1>Vitrine Uberlândia</h1>
           <h2>Érica</h2>
-          <p className='hero-description'>
-            Desde 2018, impulsionando marcas, produtos e serviços em 
-            Uberlândia.
+          <p className="hero-description">
+            Desde 2018, impulsionando marcas, produtos e serviços em Uberlândia.
           </p>
-          <p className='hero-details'>
-            Com criatividade, estratégia e um atendimento personalizado, meu 
+          <p className="hero-details">
+            Com criatividade, estratégia e um atendimento personalizado, meu
             objetivo é conectar pessoas e negócios de forma autêntica e eficaz.
           </p>
-          <p className='hero-details'>
-            Sou especialista em Marketing Digital, apaixonada por histórias de 
-            vida e por criar conexões que transformam. Acredito no poder de 
-            trabalhar com propósito, ao lado de pessoas honestas e dedicadas, 
+          <p className="hero-details">
+            Sou especialista em Marketing Digital, apaixonada por histórias de
+            vida e por criar conexões que transformam. Acredito no poder de
+            trabalhar com propósito, ao lado de pessoas honestas e dedicadas,
             que buscam conquistar seu espaço com amor pelo que fazem.
           </p>
-            <div className='hero-actions'>
-              <button className='partner-btn' onClick={() => scrollToSection("faleConosco")}>Seja um parceiro!</button>
-            </div>
+          <div className="hero-actions">
+            <button
+              className="partner-btn"
+              onClick={() => scrollToSection("faleConosco")}
+            >
+              Seja um parceiro!
+            </button>
+          </div>
         </div>
-        <div className='hero-image'>
+        <div className="hero-image">
           <img src="/imgHeroSection.jpg" alt="Érica - Vitrine Uberlândia" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
